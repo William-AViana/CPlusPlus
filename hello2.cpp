97f9d0d502161e4da60fa64545f6b46f96f63af0
William Viana <willdev.viana@gmail.com>
2022-08-03 21:52:52 -0300

    understanding scope resolution operator

---
+++ hello2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+
+int main() {
+
+  // scope resolution operator 
+  std::cout << "Hello C++ #1" << std::endl;
+  printf("Hello C++ #2\n");
+  puts("Hello C++ #3");
+  return 0;
+}
