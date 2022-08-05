3a9733620cfc5af7790260ee22c62f5948187898
William Viana <willdev.viana@gmail.com>
2022-08-04 22:20:42 -0300

    create folder fundamentals

---
+++ fundamentals/std.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // scope resolution operator
+  std::cout << "Good night!" << std::endl;
+  cout << "Good night! #2" << endl;
+  return 0;
+}
