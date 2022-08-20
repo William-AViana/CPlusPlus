8447125df579394fc440401dbdd771b9f7f71631
William Viana <willdev.viana@gmail.com>
2022-08-20 17:40:58 -0300

    understand unary operators

---
+++ fundamentals/unary.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+  int x = 2;
+  int y = 1;
+
+  x++; // postfix
+  ++y; // prefix
+  printf("%d %d\n", x, y);
+
+  x--; //postfix
+  --y; // prefix
+  printf("%d %d\n", x, y);
+
+  // x = 2 and y = 1
+  printf("%d\n", x-- == ++y);
+  // x = 1 and y - 2
+  return 0;
+}
